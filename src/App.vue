9428ac6e88c9facbb5a208d5fcd84393a441c58d
Bryce Watson <brycewatson315@gmail.com>
2022-04-03 16:52:21 -0400

    minor updates

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,36 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <v-app>
+    <v-main>
+      
+      <hello-world/>
+    </v-main>
+  </v-app>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios';
+import HelloWorld from './components/HelloWorld.vue';
 
 export default {
+  components: { HelloWorld },
   name: 'App',
-  components: {
-    HelloWorld
+  data () {
+    return {
+      blogs: [],
+      error: null
+    }
+  },
+  async mounted () {
+    try {
+      axios.get('http://localhost:1337/api/blogs').then(response => {
+        console.log(response);
+      });
+      const response = await axios.get('http://localhost:1337/api/blogs')
+      this.blogs = response.data.data
+    } catch (error) {
+      this.error = error;
+    }
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,16 @@
+import 'vuetify/styles' // Global CSS has to be imported
 import { createApp } from 'vue'
+import { createVuetify } from 'vuetify'
 import App from './App.vue'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+const vuetify = createVuetify(
+  components,
+  directives,
+) // Replaces new Vuetify(...)
+
+app.use(vuetify)
+
+app.mount('#app')
