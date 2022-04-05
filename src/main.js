964d98a4856853f24e3d4c580425999bf40c52d4
Bryce Watson <brycewatson315@gmail.com>
2022-04-05 18:18:32 -0400

    built new landing page

--- src/main.js
+++ src/main.js
@@ -4,11 +4,22 @@ import { createVuetify } from 'vuetify'
 import App from './App.vue'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
+import { aliases, mdi } from 'vuetify/lib/iconsets/mdi-svg'
+
 
 const app = createApp(App)
 const vuetify = createVuetify(
   components,
   directives,
+  {
+    icons: {
+      defaultSet: 'mdi',
+      aliases,
+      sets: {
+        mdi,
+      },
+    },
+  }
 ) // Replaces new Vuetify(...)
 
 app.use(vuetify)
